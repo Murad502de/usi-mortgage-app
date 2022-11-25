a086a418abe1cbaf50c5a3b4af3ccdf5e4d839e4
itWelt <rassulovmurad@gmail.com>
2022-11-26 00:30:54 +0300

    [main][task] fixation commit

--- src/utils/debounce.js
+++ src/utils/debounce.js
@@ -1,7 +1,18 @@
-function debounce(func, timeout = 300) {
+// export const debounce = (func, timeout = 300) => {
+//   let timer;
+
+//   return (...args) => {
+//     clearTimeout(timer);
+
+//     timer = setTimeout(() => { func.apply(this, args); }, timeout);
+//   };
+// }
+
+
+export default function (func, timeout = 300) {
   let timer;
 
-  return (...args) => {
+  return function (...args) {
     clearTimeout(timer);
 
     timer = setTimeout(() => { func.apply(this, args); }, timeout);
--- src/views/LeadsCard.vue
+++ src/views/LeadsCard.vue
@@ -1,9 +1,10 @@
 <template lang="pug">
 .usi-mortgage--leadscard
-  MortgageButton {{ mortgageButtonTitle }} : {{ mortgageBtnShow }}
+  MortgageButton(v-show="mortgageBtnShow") {{ mortgageButtonTitle }}
 </template>
 
 <script>
+import debounce from "@/utils/debounce";
 import MortgageButton from "@/components/ui/Button";
 
 export default {
@@ -15,28 +16,37 @@ export default {
   data: () => {
     return {
       mortgageButtonTitle: "HYPOTHEK BTN 2022",
+      mortgageBtnShow: false,
     };
   },
-  computed: {
-    mortgageBtnShow() {
-      return document.querySelector('div[data-id="685555"] .icon-radio-checked input')?.value;
-    },
-  },
+  computed: {},
 
   watch: {},
   methods: {
     /* GETTERS */
     /* SETTERS */
     /* HANDLERS */
+    selectMortgage: debounce(function () {
+      console.debug("selectMortgage", this);
+
+      this.mortgageBtnShow = Number(document.querySelector('div[data-id="685555"] .icon-radio-checked input')?.value) === 319719;
+    }, 100),
     /* HELPERS */
     /* ACTIONS */
   },
 
   created() {
     console.debug("LeadsCard << created"); //DELETE
+
+    document.querySelector('div[data-id="685555"]').addEventListener("click", this.selectMortgage);
   },
   mounted() {
     console.debug("LeadsCard << mounted"); //DELETE
+  },
+  unmounted() {
+    console.debug("LeadsCard << unmounted"); //DELETE
+
+    document.querySelector('div[data-id="685555"]').removeEventListener("click", this.selectMortgage);
   },
 };
 </script>
