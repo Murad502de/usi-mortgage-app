d003fb0b8be1ccfbc6c0b336f432c120a1d59d9b
itWelt <rassulovmurad@gmail.com>
2022-11-28 18:53:54 +0300

    [main][task] AdvancedSettings refactored

--- src/views/AdvancedSettings/index.vue
+++ src/views/AdvancedSettings/index.vue
@@ -4,51 +4,5 @@
 ) USI advanced-settings page {{ workArea }} : {{ count }}
 </template>
 
-<script>
-import { teleport } from "@/utils/teleport";
-
-export default {
-  components: {},
-
-  props: {},
-  data: () => {
-    return {
-      count: 0,
-    };
-  },
-  computed: {
-    workArea() {
-      return this.$store.getters["widget/params"].widget_code
-        ? `#work-area-${this.$store.getters["widget/params"].widget_code}`
-        : null;
-    },
-  },
-
-  watch: {
-    workArea(newWorkArea, oldWorkArea) {
-      if (newWorkArea) {
-        teleport({
-          toSelector: newWorkArea,
-          elementSelector: ".usi-mortgage--advanced-settings",
-        });
-      }
-    },
-  },
-  methods: {
-    /* GETTERS */
-    /* SETTERS */
-    /* HANDLERS */
-    testClick() {
-      this.count++;
-    },
-    /* HELPERS */
-    /* ACTIONS */
-  },
-
-  created() {},
-  mounted() {},
-};
-</script>
-
-<style lang="scss">
-</style>
+<script src="./script.js" />
+<style lang="scss" src="./styles.scss" />
---
+++ src/views/AdvancedSettings/script.js
@@ -0,0 +1,43 @@
+import { teleport } from "@/utils/teleport";
+
+export default {
+  components: {},
+
+  props: {},
+  data: () => {
+    return {
+      count: 0,
+    };
+  },
+  computed: {
+    workArea() {
+      return this.$store.getters["widget/params"].widget_code
+        ? `#work-area-${this.$store.getters["widget/params"].widget_code}`
+        : null;
+    },
+  },
+
+  watch: {
+    workArea(newWorkArea, oldWorkArea) {
+      if (newWorkArea) {
+        teleport({
+          toSelector: newWorkArea,
+          elementSelector: ".usi-mortgage--advanced-settings",
+        });
+      }
+    },
+  },
+  methods: {
+    /* GETTERS */
+    /* SETTERS */
+    /* HANDLERS */
+    testClick() {
+      this.count++;
+    },
+    /* HELPERS */
+    /* ACTIONS */
+  },
+
+  created() { },
+  mounted() { },
+};
